71a17f0e3db0766b23f93c243b7d7079764345c4
AKA-Saad <saad@ztabs.co>
2023-10-08 16:04:05 +0500

    add home page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="bg-blue-500 text-white py-6 mt-14">
+        <h1 class="text-4xl font-bold text-center">Welcome to Home Page </h1>
+    </div>
+
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  // Other Tailwind CSS configuration...
+}
